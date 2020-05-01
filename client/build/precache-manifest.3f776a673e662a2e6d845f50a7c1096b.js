eb18fea317c084e788ad02513a5f8644e2f80d56
MatthewDonald98 <matthewdonald@rocketmail.com>
2020-05-01 13:51:13 +0100

    unignore

---
+++ client/build/precache-manifest.3f776a673e662a2e6d845f50a7c1096b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fe1e0b8f4c51588ebfae61f3c62685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "476848cf9d84b075345d",
+    "url": "/static/css/2.392f69cd.chunk.css"
+  },
+  {
+    "revision": "a769a7a6037e324ded85",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "476848cf9d84b075345d",
+    "url": "/static/js/2.6fe74513.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.6fe74513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a769a7a6037e324ded85",
+    "url": "/static/js/main.17224b29.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
